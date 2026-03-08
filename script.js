152159b21b61b5e4c96774d5637d3778bab93d47
utkarshjeet <utkarshyadav1029@gmail.com>
2026-03-08 11:36:00 +0530

    testing

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const score = document.querySelector('.score')
 const timer = document.querySelector('time')
 const square = document.querySelectorAll('.square')
 
+const lund= 0;
+
 
 function move () {
     
