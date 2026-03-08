1f5a5d7c4a345e79010e1435c3ced50bad670bcf
utkarshjeet <utkarshyadav1029@gmail.com>
2026-03-08 11:31:57 +0530

    added random block generation and timer not working

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const score = document.querySelector('.score')
-const timer = document.querySelector('time')
-const square = document.querySelectorAll('.square')
+const timer = document.querySelector('.time')
+const squares = document.querySelectorAll('.square')
 
+let game = null
+let time = 60
 
-function move () {
-    
+function move() {
+
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    const random = Math.floor(Math.random() * 9)
+
+    squares[random].classList.add('mole')
+
+    time--
+    timer.innerText = time
+
+    if(time == 0){
+        clearInterval(game)
+    }
 }
+
+game = setInterval(move,1000)
