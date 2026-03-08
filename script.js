ca222ff634f859fabc26c5d523c5e4bda010f299
utkarshjeet <utkarshyadav1029@gmail.com>
2026-03-08 13:05:47 +0530

    game filanalise few optional thingns can be added

--- script.js
+++ script.js
@@ -4,25 +4,46 @@ const squares = document.querySelectorAll('.square')
 
 let game = null
 let time = 60
-
+let result = 0
+let clickId 
+let random 
 
 
 function move() {
 
     squares.forEach(square => {
         square.classList.remove('mole')
+
     })
 
-    const random = Math.floor(Math.random() * 9)
+    random = Math.floor(Math.random() * 9) 
 
     squares[random].classList.add('mole')
 
     time--
     timer.innerText = time
 
+   
     if(time == 0){
         clearInterval(game)
+        result = 0;
     }
 }
+
+
+squares.forEach(square => {{
+
+    square.addEventListener('mousedown' , event => {
+       clickId = event.target.id    
+        if(random == clickId-1 )
+    {
+        result++
+        score.innerText = result
+    }
+
+    })
+    
+    
+}})
 
 game = setInterval(move,1000)
